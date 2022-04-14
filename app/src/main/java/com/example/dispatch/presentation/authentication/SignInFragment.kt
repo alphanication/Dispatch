7b8d81e8e7127320c41fa417d2b265d415281770
alphanication <usa.alphanication@gmail.com>
2022-04-14 20:52:49 +0300

    sign in user presentation-module realization

--- app/src/main/java/com/example/dispatch/presentation/authentication/SignInFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/authentication/SignInFragment.kt
@@ -1,16 +1,25 @@
 package com.example.dispatch.presentation.authentication
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.dispatch.databinding.FragmentSignInBinding
+import com.example.dispatch.domain.models.FbResponse
+import com.example.dispatch.domain.models.UserAuth
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@AndroidEntryPoint
+@ExperimentalCoroutinesApi
 class SignInFragment : Fragment() {
     private lateinit var binding: FragmentSignInBinding
     private val viewModel: SignInViewModel by viewModels()
+    private val userAuth = UserAuth(email = "", password = "")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +27,75 @@ class SignInFragment : Fragment() {
     ): View {
         binding = FragmentSignInBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.buttonSignIn.setOnClickListener {
+            if(validEditTextError()) {
+                userAuthEditTextInit()
+                loginUserAuthObserve(userAuth = userAuth)
+            }
+        }
+    }
+
+    private fun loginUserAuthObserve(userAuth: UserAuth) {
+        viewModel.loginUserAuth(userAuth = userAuth).observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is FbResponse.Loading -> {
+                    showProgressBar(showOrNo = true)
+                }
+                is FbResponse.Fail -> {
+                    showProgressBar(showOrNo = false)
+                    Toast.makeText(activity, "User auth fail :(", Toast.LENGTH_SHORT).show()
+                }
+                is FbResponse.Success -> {
+                    showProgressBar(showOrNo = false)
+                    Toast.makeText(activity, "User auth success! :)", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun validEditTextError() : Boolean {
+        val email = binding.edittextEmail.text.toString()
+        val password = binding.edittextPassword.text.toString()
+
+        var valid = false
+
+        if (email.isEmpty()) {
+            binding.edittextEmail.setError("Enter email address.", null)
+            binding.edittextEmail.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.edittextEmail.setError("Enter valid email address.", null)
+            binding.edittextEmail.requestFocus()
+        } else if (password.isEmpty()) {
+            binding.edittextPassword.setError("Enter password.", null)
+            binding.edittextPassword.requestFocus()
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+    private fun userAuthEditTextInit() {
+        userAuth.email = binding.edittextEmail.text.toString()
+        userAuth.password = binding.edittextPassword.text.toString()
+    }
+
+    private fun showProgressBar(showOrNo: Boolean) {
+        if (showOrNo) {
+            binding.progressbarSignIn.visibility = View.VISIBLE
+            binding.buttonSignIn.visibility = View.INVISIBLE
+        } else {
+            binding.progressbarSignIn.visibility = View.INVISIBLE
+            binding.buttonSignIn.visibility = View.VISIBLE
+        }
     }
 }
