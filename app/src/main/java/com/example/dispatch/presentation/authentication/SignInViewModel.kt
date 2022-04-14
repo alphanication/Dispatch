fdb370c479f731cd84954458cfe528d7c554dd83
alphanication <usa.alphanication@gmail.com>
2022-04-14 20:52:49 +0300

    sign in user presentation-module realization

--- app/src/main/java/com/example/dispatch/presentation/authentication/SignInViewModel.kt
+++ app/src/main/java/com/example/dispatch/presentation/authentication/SignInViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.dispatch.presentation.authentication
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.dispatch.domain.models.FbResponse
+import com.example.dispatch.domain.models.UserAuth
 import com.example.dispatch.domain.usecase.SignInUserAuthUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
@@ -11,5 +15,12 @@ import javax.inject.Inject
 class SignInViewModel @Inject constructor(
     private val signInUserAuthUseCase: SignInUserAuthUseCase
 ) : ViewModel() {
-
+    fun loginUserAuth(userAuth: UserAuth) = liveData(Dispatchers.IO) {
+        emit(FbResponse.Loading())
+        try {
+            signInUserAuthUseCase.execute(userAuth = userAuth).collect { emit(it) }
+        } catch (e: Exception) {
+            emit(FbResponse.Fail(e))
+        }
+    }
 }
