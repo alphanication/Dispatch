04fb78d76a5aebfbe0c096b40dab73232a6137f0
alphanication <usa.alphanication@gmail.com>
2022-04-14 16:35:57 +0300

    injection modules clean architecture, gradle setting

--- app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dispatch
+package com.example.dispatch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dispatch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
