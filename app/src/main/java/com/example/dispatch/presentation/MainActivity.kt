28c15bc81a95b559ce80de483c465a746493b2a9
alphanication <usa.alphanication@gmail.com>
2022-04-14 19:00:14 +0300

    first setting dagger-hilt in project

--- app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.dispatch.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dispatch.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
