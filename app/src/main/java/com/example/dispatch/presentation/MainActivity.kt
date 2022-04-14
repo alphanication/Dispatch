651e6ef0619aa63031eed694f2ea5ec78936cdd8
alphanication <usa.alphanication@gmail.com>
2022-04-14 19:01:26 +0300

    setup NavController MainActivity.kt

--- app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.dispatch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.dispatch.R
 import com.example.dispatch.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupNav()
+    }
+
+    private fun setupNav() {
+        navController = Navigation.findNavController(this, R.id.container_fragment)
     }
 }
