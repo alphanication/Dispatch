65212a1068a9ad603a060043f9cb605d5b14133b
alphanication <usa.alphanication@gmail.com>
2022-04-14 22:42:54 +0300

    viewBinding SignUpFragment.kt

--- app/src/main/java/com/example/dispatch/presentation/registration/SignUpFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/registration/SignUpFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.dispatch.R
+import com.example.dispatch.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
