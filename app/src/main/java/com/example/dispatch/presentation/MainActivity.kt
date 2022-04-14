144965a560bcf98281136b35103bc12e74657269
alphanication <usa.alphanication@gmail.com>
2022-04-14 18:43:30 +0300

    viewBinding MainActivity.kt

--- app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.dispatch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dispatch.R
+import com.example.dispatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
