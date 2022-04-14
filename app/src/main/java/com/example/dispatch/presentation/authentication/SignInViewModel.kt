d58ff4585499327b9166814909f04fc40c4526a4
alphanication <usa.alphanication@gmail.com>
2022-04-14 19:23:58 +0300

    dagger-hilt inject SignInViewModel.kt

--- app/src/main/java/com/example/dispatch/presentation/authentication/SignInViewModel.kt
+++ app/src/main/java/com/example/dispatch/presentation/authentication/SignInViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.dispatch.presentation.authentication
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SignInViewModel() : ViewModel() {
+@HiltViewModel
+class SignInViewModel @Inject constructor(
+
+) : ViewModel() {
 
 }
