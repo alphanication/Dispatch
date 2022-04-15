7f21cd9c34aab4b8edc922846c098af92d1593ec
alphanication <usa.alphanication@gmail.com>
2022-04-15 09:46:46 +0300

    registration settings dependency injection

--- app/src/main/java/com/example/dispatch/di/DomainModule.kt
+++ app/src/main/java/com/example/dispatch/di/DomainModule.kt
@@ -1,7 +1,8 @@
 package com.example.dispatch.di
 
 import com.example.dispatch.domain.repository.UserAuthRepository
-import com.example.dispatch.domain.usecase.SignInUserAuthUseCase
+import com.example.dispatch.domain.repository.UserDetailsRepository
+import com.example.dispatch.domain.usecase.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,5 +16,35 @@ class DomainModule {
     @Provides
     fun providesSignInUserAuthUseCase(userAuthRepository: UserAuthRepository) : SignInUserAuthUseCase {
         return SignInUserAuthUseCase(userAuthRepository = userAuthRepository)
+    }
+
+    @Provides
+    fun providesSignUpUserAuthUseCase(userAuthRepository: UserAuthRepository) : SignUpUserAuthUseCase {
+        return SignUpUserAuthUseCase(userAuthRepository = userAuthRepository)
+    }
+
+    @Provides
+    fun providesDeleteCurrentUserUseCase(userAuthRepository: UserAuthRepository) : DeleteCurrentUserUseCase {
+        return DeleteCurrentUserUseCase(userAuthRepository = userAuthRepository)
+    }
+
+    @Provides
+    fun providesGetCurrentUserUidUseCase(userAuthRepository: UserAuthRepository) : GetCurrentUserUidUseCase {
+        return GetCurrentUserUidUseCase(userAuthRepository = userAuthRepository)
+    }
+
+    @Provides
+    fun providesSaveImageProfileUseCase(userDetailsRepository: UserDetailsRepository) : SaveUserImageProfileUseCase {
+        return SaveUserImageProfileUseCase(userDetailsRepository = userDetailsRepository)
+    }
+
+    @Provides
+    fun providesDeleteUserImageProfileUseCase(userDetailsRepository: UserDetailsRepository) : DeleteUserImageProfileUseCase {
+        return DeleteUserImageProfileUseCase(userDetailsRepository = userDetailsRepository)
+    }
+
+    @Provides
+    fun providesSaveUseDetailsUseCase(userDetailsRepository: UserDetailsRepository) : SaveUserDetailsUseCase {
+        return SaveUserDetailsUseCase(userDetailsRepository = userDetailsRepository)
     }
 }
