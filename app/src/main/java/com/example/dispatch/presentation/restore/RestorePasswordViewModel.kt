5914335381aeb7d94e00facc861374ab69dd8678
alphanication <usa.alphanication@gmail.com>
2022-04-15 19:48:05 +0300

    add RestorePasswordViewModel.kt

---
+++ app/src/main/java/com/example/dispatch/presentation/restore/RestorePasswordViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.dispatch.presentation.restore
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RestorePasswordViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
