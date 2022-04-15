7b16176234e2ae8032db9c42b1915d9c92fbac26
alphanication <usa.alphanication@gmail.com>
2022-04-15 19:45:52 +0300

    viewBinding RestorePasswordFragment.kt

--- app/src/main/java/com/example/dispatch/presentation/restore/RestorePasswordFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/restore/RestorePasswordFragment.kt
@@ -1,17 +1,20 @@
 package com.example.dispatch.presentation.restore
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dispatch.R
+import androidx.fragment.app.Fragment
+import com.example.dispatch.databinding.FragmentRestorePasswordBinding
 
 class RestorePasswordFragment : Fragment() {
+    private lateinit var binding: FragmentRestorePasswordBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_restore_password, container, false)
+    ): View {
+        binding = FragmentRestorePasswordBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
