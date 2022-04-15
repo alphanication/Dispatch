ca349538b4728f7f35117585ef0ecde1460378e0
alphanication <usa.alphanication@gmail.com>
2022-04-15 19:48:05 +0300

    add RestorePasswordViewModel.kt

--- app/src/main/java/com/example/dispatch/presentation/restore/RestorePasswordFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/restore/RestorePasswordFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.dispatch.databinding.FragmentRestorePasswordBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RestorePasswordFragment : Fragment() {
     private lateinit var binding: FragmentRestorePasswordBinding
+    private val viewModel: RestorePasswordViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
