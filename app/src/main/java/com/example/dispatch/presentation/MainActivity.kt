54e349d30107420c97915a0660d6d48d43155ea6
alphanication <usa.alphanication@gmail.com>
2022-04-16 15:53:47 +0300

    show bottom navigation view in fragment_current_user_profile.xml

--- app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dispatch/presentation/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-//                R.id.idfragment -> showBottomNav()
+                R.id.currentUserProfileFragment -> showBottomNav()
                 else -> hideBottomNav()
             }
         }
