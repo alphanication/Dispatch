6ad1256d3df512e266c80de4d07ca57215074d87
alphanication <usa.alphanication@gmail.com>
2022-04-16 12:05:25 +0300

    add CurrentUserProfileViewModel.kt

--- app/src/main/java/com/example/dispatch/presentation/profile/CurrentUserProfileFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/profile/CurrentUserProfileFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.dispatch.databinding.FragmentCurrentUserProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CurrentUserProfileFragment : Fragment() {
     private lateinit var binding: FragmentCurrentUserProfileBinding
+    private val viewModel: CurrentUserProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
