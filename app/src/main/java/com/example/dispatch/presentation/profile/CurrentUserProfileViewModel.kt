3446f9f86735d87a72857d9323bde00d706e97f6
alphanication <usa.alphanication@gmail.com>
2022-04-16 12:05:25 +0300

    add CurrentUserProfileViewModel.kt

---
+++ app/src/main/java/com/example/dispatch/presentation/profile/CurrentUserProfileViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.dispatch.presentation.profile
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrentUserProfileViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
