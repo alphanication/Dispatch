82dcd50c8ff945cb366fab195b591d0b5bc2cd65
alphanication <usa.alphanication@gmail.com>
2022-04-20 19:35:13 +0300

    change email-, fullname-, dateBirth-, photoProfileUrl- UseCase (UserDetails) realization

---
+++ domain/src/main/java/com/example/dispatch/domain/usecase/ChangeUserDetailsDateBirthUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.dispatch.domain.usecase
+
+import com.example.dispatch.domain.models.FbResponse
+import com.example.dispatch.domain.repository.UserDetailsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+@ExperimentalCoroutinesApi
+class ChangeUserDetailsDateBirthUseCase(private val userDetailsRepository: UserDetailsRepository) {
+    suspend fun execute(dateBirth: String) : Flow<FbResponse<Boolean>> {
+        return userDetailsRepository.changeDateBirth(dateBirth = dateBirth)
+    }
+}
---
+++ domain/src/main/java/com/example/dispatch/domain/usecase/ChangeUserDetailsEmailUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.dispatch.domain.usecase
+
+import com.example.dispatch.domain.models.FbResponse
+import com.example.dispatch.domain.repository.UserDetailsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+@ExperimentalCoroutinesApi
+class ChangeUserDetailsEmailUseCase(private val userDetailsRepository: UserDetailsRepository) {
+    suspend fun execute(email: String) : Flow<FbResponse<Boolean>> {
+        return userDetailsRepository.changeEmailAddress(email = email)
+    }
+}
