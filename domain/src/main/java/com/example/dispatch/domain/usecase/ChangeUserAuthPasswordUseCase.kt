f193c5e5a5d195318217c4974cd6c10cfd3cdaad
alphanication <usa.alphanication@gmail.com>
2022-04-21 21:45:59 +0300

    add UserAuth to change email and password User usecase

--- domain/src/main/java/com/example/dispatch/domain/usecase/ChangeUserAuthPasswordUseCase.kt
+++ domain/src/main/java/com/example/dispatch/domain/usecase/ChangeUserAuthPasswordUseCase.kt
@@ -1,13 +1,14 @@
 package com.example.dispatch.domain.usecase
 
 import com.example.dispatch.domain.models.FbResponse
+import com.example.dispatch.domain.models.UserAuth
 import com.example.dispatch.domain.repository.UserAuthRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 
 @ExperimentalCoroutinesApi
 class ChangeUserAuthPasswordUseCase(private val userAuthRepository: UserAuthRepository) {
-    suspend fun execute(newPassword: String): Flow<FbResponse<Boolean>> {
-        return userAuthRepository.changePassword(newPassword = newPassword)
+    suspend fun execute(userAuth: UserAuth, newPassword: String): Flow<FbResponse<Boolean>> {
+        return userAuthRepository.changePassword(userAuth = userAuth, newPassword = newPassword)
     }
 }
