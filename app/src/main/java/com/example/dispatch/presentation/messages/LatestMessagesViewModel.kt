8ea83b21a18eeb6faea261c94182d3e1a5ccaaa4
alphanication <usa.alphanication@gmail.com>
2022-04-22 18:45:53 +0300

    add LatestMessagesViewModel.kt

---
+++ app/src/main/java/com/example/dispatch/presentation/messages/LatestMessagesViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.dispatch.presentation.messages
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LatestMessagesViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
