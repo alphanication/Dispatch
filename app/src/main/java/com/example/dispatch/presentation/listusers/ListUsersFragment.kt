a44cf98ad72dd8b52c641d424dd6b958051f6253
alphanication <usa.alphanication@gmail.com>
2022-04-24 10:45:29 +0300

    add ListUsersFragment.kt

---
+++ app/src/main/java/com/example/dispatch/presentation/listusers/ListUsersFragment.kt
@@ -0,0 +1,17 @@
+package com.example.dispatch.presentation.listusers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dispatch.R
+
+class ListUsersFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list_users, container, false)
+    }
+}
