58d840331fe41cb188209ca7e77a949eadc6a706
alphanication <usa.alphanication@gmail.com>
2022-04-24 22:19:53 +0300

    add splash screen

---
+++ app/src/main/java/com/example/dispatch/presentation/splash/SplashScreenFragment.kt
@@ -0,0 +1,26 @@
+package com.example.dispatch.presentation.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.dispatch.R
+
+class SplashScreenFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_splash_screen, container, false)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_signInFragment)
+        }, 800)
+
+        return view
+    }
+}
