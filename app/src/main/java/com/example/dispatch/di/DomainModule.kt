35e41d0fdc3a46909b69a0bc2f32826e22033d05
alphanication <usa.alphanication@gmail.com>
2022-04-24 12:52:25 +0300

    GetUsersListUseCase.kt di

--- app/src/main/java/com/example/dispatch/di/DomainModule.kt
+++ app/src/main/java/com/example/dispatch/di/DomainModule.kt
@@ -108,4 +108,9 @@ class DomainModule {
     fun providesChangeUserDetailsPhotoProfileUrlUseCase(userDetailsRepository: UserDetailsRepository): ChangeUserDetailsPhotoProfileUrlUseCase {
         return ChangeUserDetailsPhotoProfileUrlUseCase(userDetailsRepository = userDetailsRepository)
     }
+
+    @Provides
+    fun providesGetUsersListUseCase(userDetailsRepository: UserDetailsRepository) : GetUsersListUseCase {
+        return GetUsersListUseCase(userDetailsRepository = userDetailsRepository)
+    }
 }
