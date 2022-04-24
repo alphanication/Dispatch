7a89aecc71dcec03a99aca063f8c3a15cd6596d2
alphanication <usa.alphanication@gmail.com>
2022-04-24 12:49:51 +0300

    add GetUsersListUseCase.kt

---
+++ domain/src/main/java/com/example/dispatch/domain/usecase/GetUsersListUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.dispatch.domain.usecase
+
+import com.example.dispatch.domain.models.FbResponse
+import com.example.dispatch.domain.models.UserDetails
+import com.example.dispatch.domain.repository.UserDetailsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+@ExperimentalCoroutinesApi
+class GetUsersListUseCase(private val userDetailsRepository: UserDetailsRepository) {
+    suspend fun execute(): Flow<FbResponse<UserDetails>> {
+        return userDetailsRepository.getUsersList()
+    }
+}
