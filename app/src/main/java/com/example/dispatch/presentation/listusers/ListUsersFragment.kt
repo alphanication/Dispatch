0bed38528cc06a96087f185b2441ae0c6acf1b47
alphanication <usa.alphanication@gmail.com>
2022-04-24 10:54:10 +0300

    add ListUsersViewModel.kt

--- app/src/main/java/com/example/dispatch/presentation/listusers/ListUsersFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/listusers/ListUsersFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.dispatch.R
 import com.example.dispatch.databinding.FragmentListUsersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ListUsersFragment : Fragment() {
     private lateinit var binding: FragmentListUsersBinding
+    private val viewModel: ListUsersViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
---
+++ app/src/main/java/com/example/dispatch/presentation/listusers/ListUsersViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.dispatch.presentation.listusers
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListUsersViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
