fa02b1f7bd95c153321abfb7c3a2a453c69da6b3
alphanication <usa.alphanication@gmail.com>
2022-04-25 21:48:54 +0300

    add DetailsMessagesViewModel.kt

--- app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.dispatch.R
 import com.example.dispatch.databinding.FragmentDetailsMessagesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsMessagesFragment : Fragment() {
     private lateinit var binding: FragmentDetailsMessagesBinding
+    private val viewModel: DetailsMessagesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
