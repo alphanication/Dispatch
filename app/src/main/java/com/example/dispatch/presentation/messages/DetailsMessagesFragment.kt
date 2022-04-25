90e9396eeabcf4ec2149647eccc17387335751b6
alphanication <usa.alphanication@gmail.com>
2022-04-25 23:26:08 +0300

    passing selected user uid ListUsersFragment.kt to DetailsMessagesFragment.kt

--- app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesFragment.kt
@@ -1,6 +1,7 @@
 package com.example.dispatch.presentation.messages
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.dispatch.R
 import com.example.dispatch.databinding.FragmentDetailsMessagesBinding
+import com.example.dispatch.presentation.listusers.ListUsersFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
+@ExperimentalCoroutinesApi
 class DetailsMessagesFragment : Fragment() {
     private lateinit var binding: FragmentDetailsMessagesBinding
     private val viewModel: DetailsMessagesViewModel by viewModels()
+    private lateinit var userPartnerUid: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +26,10 @@ class DetailsMessagesFragment : Fragment() {
     ): View {
         binding = FragmentDetailsMessagesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        userPartnerUid = requireArguments().getString(ListUsersFragment.PARTNER_UID).toString()
     }
 }
