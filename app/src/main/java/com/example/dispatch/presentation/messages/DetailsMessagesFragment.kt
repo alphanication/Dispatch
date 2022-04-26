52f6ee40e1fd9c7d90b2b5fcca5855851d8182f6
alphanication <usa.alphanication@gmail.com>
2022-04-26 12:29:47 +0300

    show partnerUser details

--- app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesFragment.kt
+++ app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesFragment.kt
@@ -1,16 +1,18 @@
 package com.example.dispatch.presentation.messages
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.dispatch.R
 import com.example.dispatch.databinding.FragmentDetailsMessagesBinding
+import com.example.dispatch.domain.models.FbResponse
 import com.example.dispatch.presentation.listusers.ListUsersFragment
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
@@ -18,7 +20,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 class DetailsMessagesFragment : Fragment() {
     private lateinit var binding: FragmentDetailsMessagesBinding
     private val viewModel: DetailsMessagesViewModel by viewModels()
-    private lateinit var userPartnerUid: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +31,39 @@ class DetailsMessagesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        userPartnerUid = requireArguments().getString(ListUsersFragment.PARTNER_UID).toString()
+        viewModel._companionUid.value =
+            requireArguments().getString(ListUsersFragment.PARTNER_UID).toString()
+        partnerUidObserve()
+        partnerDetailsObserve()
+    }
+
+    private fun partnerDetailsObserve() {
+        viewModel.companionDetails.observe(viewLifecycleOwner) { partnerDetails ->
+            binding.textViewCompanionFullname.text = partnerDetails.fullname
+            Picasso.get().load(partnerDetails?.photoProfileUrl.toString()).transform(CropCircleTransformation())
+                .into(binding.imageViewCompanionProfileImage)
+        }
+    }
+
+    private fun partnerUidObserve() {
+        viewModel.companionUid.observe(viewLifecycleOwner) { user ->
+            if (user.isNotEmpty()) {
+                val uid = viewModel.companionUid.value.toString()
+                getUserDetailsPublicOnUidObserve(uid = uid)
+            }
+        }
+    }
+
+    private fun getUserDetailsPublicOnUidObserve(uid: String) {
+        viewModel.getUserDetailsPublicOnUid(uid = uid).observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is FbResponse.Fail -> {
+                    Toast.makeText(activity, "Load user info false :( ", Toast.LENGTH_SHORT).show()
+                }
+                is FbResponse.Success -> {
+                    viewModel._companionDetails.value = result.data
+                }
+            }
+        }
     }
 }
