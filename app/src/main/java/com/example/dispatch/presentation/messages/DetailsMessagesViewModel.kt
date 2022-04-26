cb496d064ad49e99bd9e61082070476f2407b353
alphanication <usa.alphanication@gmail.com>
2022-04-26 12:29:47 +0300

    show partnerUser details

--- app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesViewModel.kt
+++ app/src/main/java/com/example/dispatch/presentation/messages/DetailsMessagesViewModel.kt
@@ -1,12 +1,33 @@
 package com.example.dispatch.presentation.messages
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.dispatch.domain.models.FbResponse
+import com.example.dispatch.domain.models.UserDetailsPublic
+import com.example.dispatch.domain.usecase.GetUserDetailsPublicOnUidUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
 @HiltViewModel
+@ExperimentalCoroutinesApi
 class DetailsMessagesViewModel @Inject constructor(
-
+    private val getUserDetailsPublicOnUidUseCase: GetUserDetailsPublicOnUidUseCase
 ) : ViewModel() {
+    val _companionUid = MutableLiveData<String>()
+    val companionUid: LiveData<String> = _companionUid
+    val _companionDetails = MutableLiveData<UserDetailsPublic>()
+    val companionDetails: LiveData<UserDetailsPublic> = _companionDetails
 
+    fun getUserDetailsPublicOnUid(uid: String) = liveData(Dispatchers.IO) {
+        emit(FbResponse.Loading())
+        try {
+            getUserDetailsPublicOnUidUseCase.execute(uid = uid).collect { emit(it) }
+        } catch (e: Exception) {
+            emit(FbResponse.Fail(e = e))
+        }
+    }
 }
