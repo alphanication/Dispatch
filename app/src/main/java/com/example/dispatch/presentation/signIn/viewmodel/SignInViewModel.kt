67e1746cbc9dd7ec1070260f205d4bff215eb24f
alphanication <usa.alphanication@gmail.com>
2022-04-27 22:51:59 +0300

    SignIn refactor, add SignInContract

--- app/src/main/java/com/example/dispatch/presentation/signIn/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/example/dispatch/presentation/signIn/viewmodel/SignInViewModel.kt
@@ -1,11 +1,13 @@
-package com.example.dispatch.presentation.authentication
+package com.example.dispatch.presentation.signIn.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.example.dispatch.domain.models.Response
 import com.example.dispatch.domain.models.UserAuth
 import com.example.dispatch.domain.usecase.CheckUserAuthSignedInUseCase
 import com.example.dispatch.domain.usecase.SignInUserAuthUseCase
+import com.example.dispatch.presentation.signIn.SignInContract
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,8 +18,8 @@ import javax.inject.Inject
 class SignInViewModel @Inject constructor(
     private val signInUserAuthUseCase: SignInUserAuthUseCase,
     private val checkUserAuthSignedInUseCase: CheckUserAuthSignedInUseCase
-) : ViewModel() {
-    fun loginUserAuth(userAuth: UserAuth) = liveData(Dispatchers.IO) {
+) : ViewModel(), SignInContract.SignInViewModel {
+    override fun signInUserAuth(userAuth: UserAuth): LiveData<Response<Boolean>> = liveData(Dispatchers.IO) {
         emit(Response.Loading())
         try {
             signInUserAuthUseCase.execute(userAuth = userAuth).collect { emit(it) }
@@ -26,7 +28,7 @@ class SignInViewModel @Inject constructor(
         }
     }
 
-    fun checkSignIn() = liveData {
+    override fun checkUserAuthSignedIn(): LiveData<Response<Boolean>> = liveData {
         emit(Response.Loading())
         try {
             checkUserAuthSignedInUseCase.execute().collect { emit(it) }
