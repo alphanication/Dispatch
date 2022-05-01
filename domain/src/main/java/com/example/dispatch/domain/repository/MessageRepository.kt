b8949eee7d78599518d8f75057fd24b42fb805f7
alphanication <usa.alphanication@gmail.com>
2022-05-01 12:17:33 +0300

    add SaveMessageUseCase, MessageRepository domain

---
+++ domain/src/main/java/com/example/dispatch/domain/repository/MessageRepository.kt
@@ -0,0 +1,11 @@
+package com.example.dispatch.domain.repository
+
+import com.example.dispatch.domain.models.Message
+import com.example.dispatch.domain.models.Response
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+@ExperimentalCoroutinesApi
+interface MessageRepository {
+    suspend fun save(message: Message) : Flow<Response<Boolean>>
+}
