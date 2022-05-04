9256d2415605918f616c32e920b9863a7d05bc0d
alphanication <usa.alphanication@gmail.com>
2022-05-04 03:27:49 +0300

    change model Message (russianMessage, englishMessage)

--- domain/src/main/java/com/example/dispatch/domain/models/Message.kt
+++ domain/src/main/java/com/example/dispatch/domain/models/Message.kt
@@ -1,7 +1,8 @@
 package com.example.dispatch.domain.models
 
 data class Message(
-    val message: String = "",
+    val russianMessage: String = "",
+    val englishMessage: String = "",
     val timestamp: Long = 0,
     val fromUserUid: String = "",
     val toUserUid: String = ""
