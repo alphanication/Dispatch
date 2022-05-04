56bb9144cbba26eeca5ff0b528fbf48361cd639e
alphanication <usa.alphanication@gmail.com>
2022-05-04 11:55:45 +0300

    language identifier data-module realization

--- data/src/main/java/com/example/dispatch/data/storage/TranslateStorage.kt
+++ data/src/main/java/com/example/dispatch/data/storage/TranslateStorage.kt
@@ -11,4 +11,6 @@ interface TranslateStorage {
     suspend fun translateRussianEnglishText(text: String) : Flow<Response<String>>
 
     suspend fun translateEnglishRussianText(text: String) : Flow<Response<String>>
+
+    suspend fun languageIndentifier(text: String) : Flow<Response<String>>
 }
