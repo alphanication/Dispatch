3a30d5612b8a55e800035aefd53b9326583cbdb5
alphanication <usa.alphanication@gmail.com>
2022-05-08 19:10:44 +0300

    RestorePassword refactor, RestorePasswordContract comments

--- app/src/main/java/com/example/dispatch/presentation/restorePassword/RestorePasswordContract.kt
+++ app/src/main/java/com/example/dispatch/presentation/restorePassword/RestorePasswordContract.kt
@@ -1,24 +1,61 @@
 package com.example.dispatch.presentation.restorePassword
 
-import androidx.lifecycle.LiveData
-import com.example.dispatch.domain.models.Response
-
 interface RestorePasswordContract {
     interface RestorePasswordFragment {
+        /**
+         * Sets the required setOnClickListener on the views fragment
+         */
         fun setOnClickListeners()
 
+        /**
+         * Observer progressBarRestore LiveData from [RestorePasswordViewModel]
+         */
+        fun progressBarRestoreObserver()
+
+        /**
+         * Observer restoreSuccess LiveData from [RestorePasswordViewModel]
+         */
+        fun restoreSuccessObserver()
+
+        /**
+         * Initializes the email value with a string from the edittext
+         * @return - initializes [String] email
+         */
         fun editTextEmailInit(): String
 
-        fun restoreUserPasswordObserve(email: String)
-
+        /**
+         * Checks the edittext for valid data
+         * @return - corresponding boolean value (correct / incorrect)
+         */
         fun validEditTextShowError(): Boolean
 
+        /**
+         * Show toast Toast.LENGTH_LONG type
+         * @param text - text, shown in toast
+         */
+        fun showToastLengthLong(text: String)
+
+        /**
+         * Shows progress bar restore
+         */
         fun showProgressBarRestore()
 
+        /**
+         * Hides progress bar restore
+         */
         fun hideProgressBarRestore()
+
+        /**
+         * Navigate to pop back stack
+         */
+        fun navigateToPopBackStack()
     }
 
     interface RestorePasswordViewModel {
-        fun restoreUserByEmail(email: String): LiveData<Response<Boolean>>
+        /**
+         * Sends an email with instructions for account recovery
+         * @param email - email associated with the account
+         */
+        fun restoreUserByEmail(email: String)
     }
 }
--- app/src/main/java/com/example/dispatch/presentation/restorePassword/viewmodel/RestorePasswordViewModel.kt
+++ app/src/main/java/com/example/dispatch/presentation/restorePassword/viewmodel/RestorePasswordViewModel.kt
@@ -1,14 +1,16 @@
 package com.example.dispatch.presentation.restorePassword.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.example.dispatch.domain.models.Response
 import com.example.dispatch.domain.usecase.RestoreUserByEmailUseCase
 import com.example.dispatch.presentation.restorePassword.RestorePasswordContract
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,11 +18,28 @@ import javax.inject.Inject
 class RestorePasswordViewModel @Inject constructor(
     private val restoreUserByEmailUseCase: RestoreUserByEmailUseCase
 ) : ViewModel(), RestorePasswordContract.RestorePasswordViewModel {
-    override fun restoreUserByEmail(email: String) : LiveData<Response<Boolean>> = liveData(Dispatchers.IO) {
-        try {
-            restoreUserByEmailUseCase.execute(email = email).collect { emit(it) }
-        } catch (e: Exception) {
-            emit(Response.Fail(e = e))
+
+    private val _progressBarRestore = MutableLiveData<Boolean>()
+    val progressBarRestore: LiveData<Boolean> = _progressBarRestore
+
+    private val _restoreSuccess = MutableLiveData<Response<Boolean>>()
+    val restoreSuccess: LiveData<Response<Boolean>> = _restoreSuccess
+
+    override fun restoreUserByEmail(email: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            restoreUserByEmailUseCase.execute(email = email).collect { result ->
+                when (result) {
+                    is Response.Loading -> _progressBarRestore.postValue(true)
+                    is Response.Fail -> {
+                        _progressBarRestore.postValue(false)
+                        _restoreSuccess.postValue(Response.Fail(e = result.e))
+                    }
+                    is Response.Success -> {
+                        _progressBarRestore.postValue(false)
+                        _restoreSuccess.postValue(Response.Success(data = true))
+                    }
+                }
+            }
         }
     }
 }
