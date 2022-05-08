413c222ade5a1d0ea7fe9ad922eac2957882c873
alphanication <usa.alphanication@gmail.com>
2022-05-08 18:48:52 +0300

    SignIn refactor, SignInContract comments

--- app/src/main/java/com/example/dispatch/presentation/signIn/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/example/dispatch/presentation/signIn/viewmodel/SignInViewModel.kt
@@ -1,8 +1,9 @@
 package com.example.dispatch.presentation.signIn.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.example.dispatch.domain.models.Response
 import com.example.dispatch.domain.models.UserAuth
 import com.example.dispatch.domain.usecase.CheckUserAuthSignedInUseCase
@@ -11,6 +12,7 @@ import com.example.dispatch.presentation.signIn.SignInContract
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,19 +21,44 @@ class SignInViewModel @Inject constructor(
     private val signInUserAuthUseCase: SignInUserAuthUseCase,
     private val checkUserAuthSignedInUseCase: CheckUserAuthSignedInUseCase
 ) : ViewModel(), SignInContract.SignInViewModel {
-    override fun signInUserAuth(userAuth: UserAuth): LiveData<Response<Boolean>> = liveData(Dispatchers.IO) {
-        try {
-            signInUserAuthUseCase.execute(userAuth = userAuth).collect { emit(it) }
-        } catch (e: Exception) {
-            emit(Response.Fail(e = e))
+
+    private val _progressBarSignIn = MutableLiveData<Boolean>()
+    val progressBarSignIn: LiveData<Boolean> = _progressBarSignIn
+
+    private val _signInSuccess = MutableLiveData<Response<Boolean>>()
+    val signInSuccess: LiveData<Response<Boolean>> = _signInSuccess
+
+    init {
+        checkUserAuthSignedIn()
+    }
+
+    override fun signInUserAuth(userAuth: UserAuth) {
+        viewModelScope.launch(Dispatchers.IO) {
+            signInUserAuthUseCase.execute(userAuth = userAuth).collect { result ->
+                when (result) {
+                    is Response.Loading -> _progressBarSignIn.postValue(true)
+                    is Response.Fail -> {
+                        _progressBarSignIn.postValue(false)
+                        _signInSuccess.postValue(Response.Fail(e = result.e))
+                    }
+                    is Response.Success -> {
+                        _progressBarSignIn.postValue(false)
+                        _signInSuccess.postValue(Response.Success(data = true))
+                    }
+                }
+            }
         }
     }
 
-    override fun checkUserAuthSignedIn(): LiveData<Response<Boolean>> = liveData {
-        try {
-            checkUserAuthSignedInUseCase.execute().collect { emit(it) }
-        } catch (e: Exception) {
-            emit(Response.Fail(e = e))
+    override fun checkUserAuthSignedIn() {
+        viewModelScope.launch(Dispatchers.IO) {
+            checkUserAuthSignedInUseCase.execute().collect { result ->
+                when (result) {
+                    is Response.Loading -> {}
+                    is Response.Fail -> {}
+                    is Response.Success -> _signInSuccess.postValue(Response.Success(data = true))
+                }
+            }
         }
     }
 }
