f100aa9bb40adc02db1b8f1ea7b9e0205854f718
alphanication <usa.alphanication@gmail.com>
2022-05-09 16:32:26 +0300

    LatestMessages refactor, LatestMessagesContract comments

--- app/src/main/java/com/example/dispatch/presentation/latestMessages/LatestMessagesContract.kt
+++ app/src/main/java/com/example/dispatch/presentation/latestMessages/LatestMessagesContract.kt
@@ -6,24 +6,62 @@ import com.example.dispatch.domain.models.UserDetails
 
 interface LatestMessagesContract {
     interface LatestMessagesFragment {
+        /**
+         * Sets the required setOnClickListener on the views fragment
+         */
         fun setOnClickListeners()
 
-        fun getCurrentUserDetailsObserver()
-
-        fun showProgressBarLoadUserDetails()
-
-        fun hideProgressBarLoadUserDetails()
-
-        fun navigateToListUsersFragment()
-
+        /**
+         * Observer userDetails LiveData from [LatestMessagesViewModel]
+         */
         fun userDetailsObserver()
 
-        fun downloadLangRussianEnglishPackObserver()
+        /**
+         * Observer progressBarLoadUserDetails LiveData from [LatestMessagesViewModel]
+         */
+        fun progressBarLoadUserDetailsObserver()
+
+        /**
+         * Observer loadCurrentUserDetailsSuccess LiveData from [LatestMessagesViewModel]
+         */
+        fun loadCurrentUserDetailsSuccessObserver()
+
+        /**
+         * Observer loadRussianEnglishPack LiveData from [LatestMessagesViewModel]
+         */
+        fun loadRussianEnglishPackObserver()
+
+        /**
+         * Shows progress bar load user details
+         */
+        fun showProgressBarLoadUserDetails()
+
+        /**
+         * Hides progress bar load user details
+         */
+        fun hideProgressBarLoadUserDetails()
+
+        /**
+         * Show toast Toast.LENGTH_LONG type
+         * @param text - text, shown in toast
+         */
+        fun showToastLengthLong(text: String)
+
+        /**
+         * Navigate to ListUsersFragment
+         */
+        fun navigateToListUsersFragment()
     }
 
     interface LatestMessagesViewModel {
-        fun getCurrentUserDetails(): LiveData<Response<UserDetails>>
+        /**
+         * Getting the details of the currently logged in user
+         */
+        fun getCurrentUserDetails()
 
-        fun downloadLangRussianEnglishPack() : LiveData<Response<Boolean>>
+        /**
+         * Download language ru-en pack (ml kit translate)
+         */
+        fun downloadLangRussianEnglishPack()
     }
 }
