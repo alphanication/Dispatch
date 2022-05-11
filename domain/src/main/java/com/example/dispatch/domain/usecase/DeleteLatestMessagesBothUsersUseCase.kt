52f26e654a91fd54e9cd351c61fca1420f76fc25
alphanication <usa.alphanication@gmail.com>
2022-05-11 12:23:49 +0300

    add deleteLatestMessagesBothUsers() MessageRepository + usecase

---
+++ domain/src/main/java/com/example/dispatch/domain/usecase/DeleteLatestMessagesBothUsersUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.dispatch.domain.usecase
+
+import com.example.dispatch.domain.models.FromToUser
+import com.example.dispatch.domain.models.Response
+import com.example.dispatch.domain.repository.MessageRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+@ExperimentalCoroutinesApi
+class DeleteLatestMessagesBothUsersUseCase(private val messageRepository: MessageRepository) {
+    suspend fun execute(fromToUser: FromToUser) : Flow<Response<Boolean>> {
+        return messageRepository.deleteLatestMessageBothUsers(fromToUser = fromToUser)
+    }
+}
