18ee614775008cf76254df363cd66d9b9f1566db
alphanication <usa.alphanication@gmail.com>
2022-05-11 14:45:24 +0300

    getLatestMessages() MessageRepository + usecase (domain-module)

---
+++ domain/src/main/java/com/example/dispatch/domain/usecase/GetLatestMessagesUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.dispatch.domain.usecase
+
+import com.example.dispatch.domain.models.Message
+import com.example.dispatch.domain.models.Response
+import com.example.dispatch.domain.repository.MessageRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+
+@ExperimentalCoroutinesApi
+class GetLatestMessagesUseCase(private val messageRepository: MessageRepository) {
+    suspend fun execute(fromUserUid: String) : Flow<Response<ArrayList<Message>>> {
+        return messageRepository.getLatestMessages(fromUserUid = fromUserUid)
+    }
+}
