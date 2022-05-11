81ea1e6906e8d4f671f7f42b3e011f4913dc6198
alphanication <usa.alphanication@gmail.com>
2022-05-11 11:34:38 +0300

    add LatestMessage item

---
+++ app/src/main/java/com/example/dispatch/presentation/latestMessages/view/LatestMessage.kt
@@ -0,0 +1,32 @@
+package com.example.dispatch.presentation.latestMessages.view
+
+import android.view.View
+import com.example.dispatch.R
+import com.example.dispatch.databinding.ItemContainerLatestMessageBinding
+import com.example.dispatch.domain.models.Message
+import com.example.dispatch.domain.models.UserDetailsPublic
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.viewbinding.BindableItem
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
+
+class LatestMessage(
+    private val message: Message,
+    private val companionUser: UserDetailsPublic
+) : BindableItem<ItemContainerLatestMessageBinding>() {
+    override fun bind(viewBinding: ItemContainerLatestMessageBinding, position: Int) {
+        viewBinding.latestMessage.text = message.englishMessage
+        viewBinding.textViewUserName.text = companionUser.fullname
+        if (companionUser.photoProfileUrl.isNotEmpty()) {
+            Picasso.get().load(companionUser.photoProfileUrl).transform(CropCircleTransformation())
+                .into(viewBinding.imageViewUserPhoto)
+        }
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.item_container_latest_message
+    }
+
+    override fun initializeViewBinding(view: View): ItemContainerLatestMessageBinding {
+        return ItemContainerLatestMessageBinding.bind(view)
+    }
+}
